f6beee41134da23b5f7f5cd4a8fa7308a9788452
hermawanumdp <rachmat.nur91@gmail.com>
2024-10-23 20:25:46 +0700

    REST API

--- express-mvc/app_server/models/db.js
+++ express-mvc/app_server/models/db.js
@@ -1,15 +1,20 @@
 let mongoose = require("mongoose");
-let dbURI = "mongodb://localhost:27017/pawII-si5c";
+//let dbURI = "mongodb://localhost:27017";
+let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2";
 
-mongoose.connect(dbURI, {
-    //useNewUrlParser: true
+mongoose.connect(dbURI,{
+    //useNewURLParser: true
 });
+
 mongoose.connection.on("connected", () => {
-    console.log("Connected to MongoDB");
+    console.log("Connected To MongoDB");
 });
+
 mongoose.connection.on("error", (error) => {
-    console.log("Connection Error : " + error);
+    console.log("Connection Error: " + error);
 });
-mongoose.connection.on("disconnected", () => {
-    console.log("Disconnected from MongoDB");
+
+mongoose.connection.on("disconected", () => {
+    console.log("Disconnected From MongoDB");
 });
+
