319f635b228f059a61e86a21de18a5a1d77a8b06
Nur Rachmat <rachmat.nur91@gmail.com>
2024-11-13 20:24:19 +0700

    Angular Directive

--- express-mvc/app_server/models/db.js
+++ express-mvc/app_server/models/db.js
@@ -1,6 +1,8 @@
 let mongoose = require("mongoose");
+
 //let dbURI = "mongodb://localhost:27017";
 let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2";
+
 
 mongoose.connect(dbURI,{
     //useNewURLParser: true
