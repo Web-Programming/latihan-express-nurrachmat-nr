483b5bdcef6da1e2aeae961542f0e03be3a06a2e
Nur Rachmat <rachmat.nur91@gmail.com>
2024-11-13 20:25:40 +0700

    Fix Conflict

--- express-mvc/app_server/models/db.js
+++ express-mvc/app_server/models/db.js
@@ -1,9 +1,4 @@
 let mongoose = require("mongoose");
-<<<<<<< HEAD
-//let dbURI = "mongodb://localhost:27017/pawII-si5c";
-let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2"
-=======
-//let dbURI = "mongodb://localhost:27017";
 let dbURI = "mongodb+srv://paw2:si@paw2.iendmj6.mongodb.net/PAWII-SI?retryWrites=true&w=majority&appName=paw2";
 
 mongoose.connect(dbURI,{
